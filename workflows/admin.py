4e469c202f12a43f7bfa7290641ba954d47f5422
Abdul12majid <yisaabdulmajid@gmail.com>
2026-06-19 01:44:49 +0100

    added workflow models

--- workflows/admin.py
+++ workflows/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Workflow, WorkflowStep
 
-# Register your models here.
+
+class WorkflowStepInline(admin.TabularInline):
+    model = WorkflowStep
+    extra = 1
+
+
+@admin.register(Workflow)
+class WorkflowAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "active", "created_at")
+    inlines = [WorkflowStepInline]
+
+
+@admin.register(WorkflowStep)
+class WorkflowStepAdmin(admin.ModelAdmin):
+    list_display = ("id","workflow","order","step_type",)
--- workflows/models.py
+++ workflows/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class Workflow(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class WorkflowStep(models.Model):
+
+    STEP_TYPES = [
+        ("extract_data", "Extract Data"),
+        ("create_record", "Create Record"),
+        ("send_email", "Send Email"),
+        ("generate_report", "Generate Report"),
+    ]
+
+    workflow = models.ForeignKey(
+        Workflow,
+        on_delete=models.CASCADE,
+        related_name="steps"
+    )
+
+    order = models.PositiveIntegerField()
+
+    step_type = models.CharField(
+        max_length=50,
+        choices=STEP_TYPES
+    )
+
+    config = models.JSONField(default=dict)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["order"]
+
+    def __str__(self):
+        return f"{self.workflow.name} - Step {self.order}"
