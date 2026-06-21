2be9b0663e9681386bb9df767b105dc37f32c511
Abdul12majid <yisaabdulmajid@gmail.com>
2026-06-21 17:52:54 +0100

    Add API for workflow steps

--- workflows/urls.py
+++ workflows/urls.py
@@ -3,4 +3,6 @@ from .views import workflow_list
 
 urlpatterns = [
     path("", workflow_list, name="workflow-list"),
+    path("<int:workflow_id>/steps/", views.workflow_steps, name="workflow-steps"),
+    path("steps/<int:step_id>/", views.delete_step, name="delete-step"),
 ]
--- workflows/views.py
+++ workflows/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from .models import Workflow
-from .serializers import WorkflowSerializer
+from .serializers import WorkflowSerializer, WorkflowStepSerializer
 
 
 @api_view(["GET", "POST"])
@@ -21,3 +21,31 @@ def workflow_list(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["GET", "POST"])
+def workflow_steps(request, workflow_id):
+
+    workflow = get_object_or_404(Workflow, id=workflow_id)
+    if request.method == "GET":
+
+        serializer = WorkflowStepSerializer(workflow.steps.all(), many=True)
+        return Response(serializer.data)
+
+    data = request.data.copy()
+    data["workflow"] = workflow.id
+
+    serializer = WorkflowStepSerializer(data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["DELETE"])
+def delete_step(request, step_id):
+
+    step = get_object_or_404(WorkflowStep, id=step_id)
+    step.delete()
+    return Response({"message": "Step deleted."})
